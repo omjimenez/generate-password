6c44fc86f40371168a2cebb1567fcc7ec0cf86b8
orjimene <orjimene@gmail.com>
2022-01-23 15:07:40 -0500

    the function generate_password is improves

--- generador_contrasena.py
+++ generador_contrasena.py
@@ -1,29 +1,32 @@
 import random
 
-def generar_contrasena():
+def generate_password():
     mayusculas = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
     minusculas = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
     simbolos = ['!', '#', '$', '&', '/', '(', ')']
     numeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
-    caracteres = mayusculas + minusculas + simbolos + numeros
+    # caracteres = mayusculas + minusculas + simbolos + numeros
 
-    contrasena = []
+    # range 33 to 126 decimal ascii table
+    characters = [chr(dec_ascii) for dec_ascii in range(33,126)]
+
+    password = []
 
     for i in range(15):
-        caracter_randon = random.choice(caracteres)
-        contrasena.append(caracter_randon)
+        character_random = random.choice(characters)
+        password.append(character_random)
 
-    contrasena = "".join(contrasena)
-    return contrasena
+    generate_password = "".join(password)
+    return generate_password
 
 
 def run():
-    contrasena = generar_contrasena()
+    password = generate_password()
 
     print('\n ************************************* \n')
     print(' ******* GENERADOR DE PASSWORD *******')
-    print('\n Tu nueva Password es: '+ contrasena)
+    print('\n Tu nueva Password es: '+ password)
     print('\n ************************************* \n')
 
 
